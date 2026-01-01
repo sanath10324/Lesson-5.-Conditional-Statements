c508d6e62fa6125058bda85f6a2982d20b260275
sanath10324 <sanathkatkamgamer@gmail.com>
2026-01-01 15:13:04 +0400

    Check if Summer or Winter

---
+++ summer_or_winter.py
@@ -0,0 +1,5 @@
+temparature = int(input("Enter the current temparature of your area:"))
+if (temparature < 20):
+    print("It is Winter in your area")
+else:
+    print("It is Summer in your area")
